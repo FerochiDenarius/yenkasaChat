9793064fe84bc6b9585d2e719dd25fce01fd9bc1
KofiBright <ofosumenyabrightkofi@gmail.com>
2026-04-09 21:53:26 +0000

    updated the admin.js

--- yenkasaChatBackend/RegLoginBackend/store/yenkasa-store-server.js
+++ yenkasaChatBackend/RegLoginBackend/store/yenkasa-store-server.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const FormData = require('form-data');
 
 const upload = multer();
-const API_BASE = process.env.TRICIABALES_API_BASE || 'http://127.0.0.1:8080';
+const API_BASE = process.env.TRICIABALES_API_BASE || 'http://134.209.182.39:8080';
 
 function forwardHeaders(req, extraHeaders = {}) {
   const headers = { ...extraHeaders };

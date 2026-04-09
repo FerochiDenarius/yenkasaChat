62a97560611a55f1df8c7b9ec7f8ace2af49489d
KofiBright <ofosumenyabrightkofi@gmail.com>
2026-04-09 21:35:08 +0000

    updated the admin.js

--- yenkasaChatBackend/RegLoginBackend/server.js
+++ yenkasaChatBackend/RegLoginBackend/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const fs = require('fs');
+const multer = require('multer');
 const helmet = require('helmet');
 const compression = require('compression');
 const cors = require('cors');
@@ -39,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 require('./store/yenkasa-store-server')(app);
 

d54371a42be3a5ff293d0a479f717602864d56ee
KofiBright <ofosumenyabrightkofi@gmail.com>
2026-04-28 16:24:37 +0000

    upgrading yenkasa ui

--- yenkasa-web/src/App.jsx
+++ yenkasa-web/src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";

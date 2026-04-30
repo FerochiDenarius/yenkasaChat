d66f91473195a0eacaac8c8b85678f1fdcf56cf2
KofiBright <ofosumenyabrightkofi@gmail.com>
2026-04-30 12:55:56 +0000

    upgrading yenkasa ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "xyz.yenkasa.app"
         minSdk = 21
         targetSdk = 35
-        versionCode = 34
-        versionName = "0.2.8"
+        versionCode = 35
+        versionName = "0.2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }

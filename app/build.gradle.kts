7d9b6946db1d5170097b01d09bc6d3512987b4c2
KofiBright <ofosumenyabrightkofi@gmail.com>
2026-05-07 21:13:45 +0000

    Introducing Yenkasa Live

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "xyz.yenkasa.app"
         minSdk = 21
         targetSdk = 35
-        versionCode = 41
-        versionName = "0.3.5"
+        versionCode = 42
+        versionName = "0.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
